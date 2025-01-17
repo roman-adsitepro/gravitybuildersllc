7697c195096a9b08d76b657a6416d3adc32091b8
Roman <roman@adsitepro.com>
2025-01-17 13:08:04 -0800

    Mailjet Setup

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,7 +1,10 @@
 <?php
 
+use Mailjet\LaravelMailjet\MailjetServiceProvider;
+
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\Filament\DashboardPanelProvider::class,
+    MailjetServiceProvider::class,
 ];
